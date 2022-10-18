c5a296384ac3c43196b87b9f209d4b27a7f6960a
vgkusuma <vgkusuma@microfocus.com>
2022-10-18 11:00:45 -0700

    Leave comment in

--- dotnet/src/MicroFocus.CafApi.QueryBuilder.Matcher/IMatcherFieldSpec.cs
+++ dotnet/src/MicroFocus.CafApi.QueryBuilder.Matcher/IMatcherFieldSpec.cs
@@ -19,6 +19,7 @@ namespace MicroFocus.CafApi.QueryBuilder.Matcher
 {
     public interface IMatcherFieldSpec<in Document>
     {
+        //[NotNull]
         IEnumerable<IMatcherFieldValue> GetFieldValues(Document document);
 
         bool IsCaseInsensitive { get; }
