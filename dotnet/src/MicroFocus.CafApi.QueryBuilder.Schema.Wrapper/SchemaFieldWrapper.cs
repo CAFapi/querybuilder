06dee1cfd17e0c635980c8eab9f55e24120bf8a3
vgkusuma <vgkusuma@microfocus.com>
2022-12-05 10:10:15 -0800

    US-610048: Implement IField wrapper

---
+++ dotnet/src/MicroFocus.CafApi.QueryBuilder.Schema.Wrapper/SchemaFieldWrapper.cs
@@ -0,0 +1,40 @@
+/**
+ * Copyright 2022 Micro Focus or one of its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+using System;
+using System.Collections.Generic;
+using MicroFocus.CafApi.QueryBuilder.Matcher;
+using MicroFocus.FAS.AdapterSdkSchema;
+
+namespace MicroFocus.CafApi.QueryBuilder.Schema.Wrapper
+{
+    public class SchemaFieldWrapper<T> : IMatcherFieldSpec<T>
+    {
+        private readonly IField _field;
+
+        public SchemaFieldWrapper(IField field)
+        {
+            _field = field;
+        }
+        public bool IsCaseInsensitive => _field.IsCaseInsensitive;
+
+        public bool IsTokenizedPath => _field.IsTokenizedPath;
+
+        public virtual IEnumerable<IMatcherFieldValue> GetFieldValues(T document)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
