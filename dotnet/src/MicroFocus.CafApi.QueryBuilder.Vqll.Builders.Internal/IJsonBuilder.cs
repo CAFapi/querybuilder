07b5d20c0ee71a862b0bb133c08389e6b8638b30
caoimhelynch <59800744+caoimhelynch@users.noreply.github.com>
2023-04-05 16:56:36 +0100

    US618048: Updating copyright headers (#6)

--- dotnet/src/MicroFocus.CafApi.QueryBuilder.Vqll.Builders.Internal/IJsonBuilder.cs
+++ dotnet/src/MicroFocus.CafApi.QueryBuilder.Vqll.Builders.Internal/IJsonBuilder.cs
@@ -1,5 +1,5 @@
 /**
- * Copyright 2022 Micro Focus or one of its affiliates.
+ * Copyright 2022-2023 Open Text.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,7 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 namespace MicroFocus.CafApi.QueryBuilder.Vqll.Builders.Internal
 {
     public interface IJsonBuilder
